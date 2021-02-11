b687c85f4bedc15d2f9f44b6c4e81c3f09cf35d5
omid <omidmmadi@gmail.com>
2021-02-11 20:49:34 +0330

    create models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Article, Category, Author
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_date', 'publish_status']
+    raw_id_fields = ('category',)
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category)
+admin.site.register(Author)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Author(models.Model):
+    avatar = models.ImageField(upload_to='files/images/avatar/', blank=True)
+    name = models.CharField(max_length=128, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    publish_choices = (
+        ('p', 'Publish'),
+        ('d', 'Draft'),
+    )
+
+    title = models.CharField(max_length=128, null=False, blank=False)
+    cover = models.ImageField(upload_to='files/images/article_cover/')
+    content = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ManyToManyField('Category')
+    publish_status = models.CharField(max_length=1, choices=publish_choices, null=False,
+                                      blank=False, default='d')
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.title
