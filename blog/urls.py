e2e099ecae8dafb3e962a914091b3025356d5481
omid <omidmmadi@gmail.com>
2021-02-12 00:45:27 +0330

    add category views

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home_page'),
     path('article/<int:article_id>', views.ContentPage.as_view(), name="content_page"),
+    path('category/<int:category_id>', views.CategoryPage.as_view(), name="category_page")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-from .models import Article
+from .models import Article, Category
+
+
 
 
 class HomePage(TemplateView):
 
     def get(self, request, format=None):
-        articles = Article.objects.filter(publish_status='p')
+        cataeorys = Category.objects.all()
+        articles = Article.objects.filter(publish_status='p').order_by('-created_date')
         context = {
-            'articles': articles
+            'articles': articles,
+            'categorys': cataeorys,
         }
 
         return render(request, 'home.html', context)
 
 
 class ContentPage(TemplateView):
+
     def get(self, request, format=None, **kwargs):
+        catagorys = Category.objects.all()
         article_id = self.kwargs.get('article_id')
         article = Article.objects.filter(publish_status='p')
         article = get_object_or_404(article, pk=article_id)
 
         context = {
             'article': article,
+            'categorys': catagorys,
         }
         return render(request, 'content.html', context)
+
+
+class CategoryPage(TemplateView):
+    def get(self, request, format=None, **kwargs):
+        category_id = self.kwargs.get('category_id')
+        articles = Article.objects.filter(category=category_id)
+        catagorys = Category.objects.all()
+        context = {
+            'articles': articles,
+            'categorys': catagorys
+        }
+
+        return render(request, 'category.html', context)
