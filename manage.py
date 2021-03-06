f57c9376c9923b1d0cd3e9a3dfedf26c7e697fd7
omid <omidmmadi@gmail.com>
2021-03-07 01:51:58 +0330

    change project name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PersonalBlog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechWithOmid.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
